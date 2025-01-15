89768c04c6f4fae68e5e01b0865b5c6232dc1d5f
Beimer Campos <beimer.developer@gmail.com>
2025-01-15 08:36:54 -0500

    lazyvim folder added

---
+++ lazy-vim/lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+keymap.set("i", "jk", "<ESC>")
+keymap.set("n", "<leader>w", ":w<CR>")
+keymap.set("n", "<leader>q", ":q<CR>")
+keymap.set("n", "<leader>Q", ":wq<CR>")
+keymap.set("n", "<leader>nh", ":nohl<CR>")
---
+++ lazy-vim/lua/plugins/ai.lua
@@ -0,0 +1,24 @@
+if true then
+  return {}
+end
+
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      -- add any opts here
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+  },
+}
