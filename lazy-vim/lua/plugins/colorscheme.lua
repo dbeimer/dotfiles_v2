5a2e94a0deb424a2f736fbce3601b9d079e3a15e
Beimer <beimercampos@yape.com.pe>
2025-01-20 18:24:37 -0500

    enable lackluster mint theme

--- lazy-vim/lua/plugins/colorscheme.lua
+++ lazy-vim/lua/plugins/colorscheme.lua
@@ -3,6 +3,47 @@ return {
     "slugbyte/lackluster.nvim",
     lazy = false,
     priority = 1000,
+    config = function()
+      local lackluster = require("lackluster")
+      lackluster.setup({
+        disable_plugin = {
+          bufferline = false,
+          cmp = false,
+          dashboard = false,
+          flash = false,
+          git_gutter = false,
+          git_signs = false,
+          headline = false,
+          indentmini = false,
+          lazy = false,
+          lightbulb = false,
+          lsp_config = false,
+          mason = false,
+          mini_diff = false,
+          navic = false,
+          noice = false,
+          notify = false,
+          oil = false,
+          rainbow_delimiter = false, -- if you want color-rainbows you should disable this
+          scollbar = false,
+          telescope = false,
+          todo_comments = false,
+          tree = false,
+          trouble = false,
+          which_key = false,
+          yanky = false,
+        },
+        tweak_syntax = {
+          comment = lackluster.color.gray4, -- or gray5
+        },
+        tweak_background = {
+          normal = "none",
+          telescope = "none",
+          menu = lackluster.color.gray3,
+          popup = "default",
+        },
+      })
+    end,
   },
   {
     "bluz71/vim-nightfly-colors",
